2369acb6798385e295b517a68a04435b53d7ef22
Melike Sinem Kaya <msinemkaya6@gmail.com>
2024-01-23 23:38:08 +0300

    feat: created the calculator method to be used

--- src/fee-calculator/Calculator.ts
+++ src/fee-calculator/Calculator.ts
@@ -1,3 +1,8 @@
-export class Calculator {
+import { Transaction } from './types';
+import { Operation } from './contracts';
 
+export class Calculator {
+  calculate(transaction : Transaction, operation : Operation) {
+    return operation.getCommissionFee(transaction);
+  }
 }
