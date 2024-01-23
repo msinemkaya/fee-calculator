15bd4cd5d14539c1333ee8e48040a27487c4af63
Melike Sinem Kaya <msinemkaya6@gmail.com>
2024-01-23 23:40:50 +0300

    feat: added JsonReader.ts

--- src/fee-calculator/readers/JsonReader.ts
+++ src/fee-calculator/readers/JsonReader.ts
@@ -1,9 +1,21 @@
-import { TransactionReader } from '../contracts'
-import { Transaction } from '../types'
+import * as fs from 'fs';
+import { TransactionReader } from '../contracts';
+import { Transaction } from '../types';
 
-export class JsonReader implements TransactionReader{
-  getTransactions() : Transaction[] {
-    return []
+export default class JsonReader implements TransactionReader {
+  constructor(private readonly filePath : string) {
   }
 
+  getTransactions() : Transaction[] {
+    let parsedData = [];
+    try {
+      const data = fs.readFileSync(this.filePath, 'utf-8');
+      parsedData = JSON.parse(data.toString());
+    } catch (e) {
+      throw e;
+      process.exit(1);
+    }
+
+    return parsedData;
+  }
 }
