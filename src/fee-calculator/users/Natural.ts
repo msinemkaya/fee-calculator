ff2396b553e55c5abd81c392efcb6b825dca6041
Melike Sinem Kaya <msinemkaya6@gmail.com>
2024-01-25 22:01:32 +0300

    feat: created natural logic and installed moment.js

--- src/fee-calculator/users/Natural.ts
+++ src/fee-calculator/users/Natural.ts
@@ -1,8 +1,23 @@
 import { Person } from '../contracts';
 import { Transaction } from '../types';
+import WeeklyTransactionChecker from '../utils/WeeklyTransactionChecker';
 
 export default class Natural implements Person {
-  calculateFee(transaction : Transaction) : number {
-    return 0;
+  constructor(private weeklyTransactionChecker : WeeklyTransactionChecker) {
+  }
+
+  getAmountToBeProcessed(transaction : Transaction) : number {
+    const { amount } = transaction.operation;
+    const weeklyData = this.weeklyTransactionChecker.getWeeklyHistory(transaction);
+    const amountHistory = weeklyData.reduce((acc, data) => acc + data.operation.amount, 0);
+    const totalAmount = amountHistory + amount;
+
+    this.weeklyTransactionChecker.addToHistory(transaction);
+    if (amountHistory <= 1000) return Math.max(0, totalAmount - 1000);
+    return amount;
+  }
+
+  getProcessedFee(calculatedAmount : number, percentage : number) : number {
+    return ((calculatedAmount * percentage) / 100);
   }
 }
