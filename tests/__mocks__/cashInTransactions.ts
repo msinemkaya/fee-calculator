942c4f2d544f1ab2470ea1ae2860c50b29b9e9b3
Melike Sinem Kaya <msinemkaya6@gmail.com>
2024-01-27 17:48:35 +0300

    feat: wrote unit test for CashIn

---
+++ tests/__mocks__/cashInTransactions.ts
@@ -0,0 +1,31 @@
+import { Transaction } from '../../src/fee-calculator/types'
+
+export const mockCashInTransactions = [
+  {
+    transaction: {
+      "operation": {
+        "amount": 1000000.00,
+        "currency": "EUR"
+      }
+    } as Transaction,
+    expected: 5.00
+  },
+  {
+    transaction: {
+      "operation": {
+        "amount": 100.00,
+        "currency": "EUR"
+      }
+    } as Transaction,
+    expected: 0.03
+  },
+  {
+    transaction: {
+      "operation": {
+        "amount": 678.34,
+        "currency": "EUR"
+      }
+    } as Transaction,
+    expected: 0.20
+  },
+]
---
+++ tests/__tests__/fee-calculator/operations/CashIn.test.ts
@@ -0,0 +1,16 @@
+import { CashIn } from '../../../../src/fee-calculator/operations'
+import { mockCashInTransactions } from '../../../__mocks__/cashInTransactions'
+
+describe('cash in transactions', () => {
+  let CashInInstance : CashIn;
+
+  beforeEach(() => {
+    CashInInstance = new CashIn(0.03, 5)
+
+  })
+
+  it.each(mockCashInTransactions)('should return calculated commission fee correctly', ({ transaction, expected }) => {
+    const result = CashInInstance.getCommissionFee(transaction)
+    expect(result).toBeCloseTo(expected)
+  })
+})
