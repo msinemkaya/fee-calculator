41dda6f2cbb431bd952ea08233446b4fc4ac1084
Melike Sinem Kaya <msinemkaya6@gmail.com>
2024-01-27 17:12:12 +0300

    feat: wrote unit test for juridical

---
+++ tests/__mocks__/juridicalTransactions.ts
@@ -0,0 +1,25 @@
+import { Transaction } from '../../src/fee-calculator/types'
+
+export const mockJuridicalTransactions : Transaction[] = [
+  {
+    "operation":
+      {
+        "amount": 52.05,
+        "currency": "EUR"
+      }
+  } as Transaction,
+  {
+    "operation":
+      {
+        "amount": 12000,
+        "currency": "EUR"
+      }
+  } as Transaction,
+  {
+    "operation":
+      {
+        "amount": 1980.73,
+        "currency": "EUR"
+      }
+  } as Transaction,
+]
